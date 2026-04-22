4a7189d54e84c1c283a78595442b737ce9bafa0e
jjsuperpower <jjs29356@gmail.com>
2026-04-21 23:18:48 -0400

    update to 25.11

--- porkbun-dns/flake.nix
+++ porkbun-dns/flake.nix
@@ -2,7 +2,7 @@
   description = "Porkbun Dynamic DNS Python Application";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     nix2container.url = "github:nlewo/nix2container";
   };
